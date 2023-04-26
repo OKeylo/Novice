23530f117304351ed2550ee78d2f51811a923a52
OKeylo <zabaev228@gmail.com>
2023-04-26 19:40:10 +0500

    Add files

---
+++ main.py
@@ -0,0 +1,30 @@
+from config import TOKEN
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+bot = Bot(token = TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply('Привет!\nИспользуй /help, чтобы узнать список доступных команд!')
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    msg = 'Я могу ответить на следующие команды:\n /photo\n /arguments\n '
+    await message.answer(msg)
+
+@dp.message_handler(commands=['photo'])
+async def process_photo_command(message: types.Message):
+    await bot.send_photo(message.from_user.id, 'https://i.ebayimg.com/images/g/LBYAAOSwCg1iikW6/s-l1600.png')
+
+@dp.message_handler(commands=['arguments'])
+async def test(message: types.Message):
+    msg = [ msg for msg in message.text.split(" ")][1::]
+    await message.answer(msg)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+    
